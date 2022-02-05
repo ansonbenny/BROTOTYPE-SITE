edd0b498d15dafc28b8cc63227f3e15ab0c01332
ansonbenny <94070164+ansonbenny@users.noreply.github.com>
2022-02-05 22:25:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".menucolor").mouseenter(function(){
+        $(this).css({"color":"orange"})
+    })
+    $(".menucolor").mouseleave(function(){
+        $(this).css({"color":"gray"})
+    })
+})
